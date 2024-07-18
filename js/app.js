113c62fc5d2547f8f180b24a70c381f5e8bc4656
Corfaya <lauracorrado0696@gmail.com>
2024-07-18 15:43:41 +0200

    [UPD] obj array created and added as data's application

--- js/app.js
+++ js/app.js
@@ -3,7 +3,36 @@ const { createApp } = Vue;
 createApp({
     data(){
         return {
-
+            todo: [
+                {
+                    text: "Chiamare il commercialista",
+                    done: false
+                },
+                {
+                    text: "Prenotare un tavolo",
+                    done: false
+                },
+                {
+                    text: "Andare dal gommista",
+                    done: false
+                },
+                {
+                    text: "Comprare gli ingredienti per stasera",
+                    done: false
+                },
+                {
+                    text: "Andare dall'estetista",
+                    done: false
+                },
+                {
+                    text: "Pulire il bagno",
+                    done: false
+                },
+                {
+                    text: "Studiare",
+                    done: false
+                }
+            ]
         }
     },
     methods: {
