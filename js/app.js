a6c4d622eb3cb96a0d271914021f6ff8b14d69e0
Corfaya <lauracorrado0696@gmail.com>
2024-07-18 16:15:20 +0200

    [UPD] Milestone 1 DONE

--- js/app.js
+++ js/app.js
@@ -1,41 +1,44 @@
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return {
-            todo: [
-                {
-                    text: "Chiamare il commercialista",
-                    done: false
-                },
-                {
-                    text: "Prenotare un tavolo",
-                    done: false
-                },
-                {
-                    text: "Andare dal gommista",
-                    done: false
-                },
-                {
-                    text: "Comprare gli ingredienti per stasera",
-                    done: false
-                },
-                {
-                    text: "Andare dall'estetista",
-                    done: false
-                },
-                {
-                    text: "Pulire il bagno",
-                    done: false
-                },
-                {
-                    text: "Studiare",
-                    done: false
-                }
-            ]
-        }
+  data() {
+    return {
+      todo: [
+        {
+          text: "Chiamare il commercialista",
+          done: false,
+        },
+        {
+          text: "Prenotare un tavolo",
+          done: false,
+        },
+        {
+          text: "Andare dal gommista",
+          done: false,
+        },
+        {
+          text: "Comprare gli ingredienti per stasera",
+          done: false,
+        },
+        {
+          text: "Andare dall'estetista",
+          done: false,
+        },
+        {
+          text: "Pulire il bagno",
+          done: false,
+        },
+        {
+          text: "Studiare",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    isTaskDone(i) {
+        this.todo[i].done = true
+        console.log(this.todo[i].done)
     },
-    methods: {
-
-    }
-}).mount("#app")
+  },
+}).mount("#app");
