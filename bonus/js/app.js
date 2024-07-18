c9b2c9884a76488fe446514130c7a885569b815b
Corfaya <lauracorrado0696@gmail.com>
2024-07-18 17:34:39 +0200

    [UPD] BONUS 2 DONE

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -33,27 +33,34 @@ createApp({
           done: false,
         },
       ],
-      newTask: undefined
+      newTask: undefined,
     };
   },
   methods: {
     isTaskDone(i) {
-        this.todo[i].done = true
-        console.log(this.todo[i].done)
+      this.todo[i].done = true;
+      console.log(this.todo[i].done);
     },
-    taskRemove(i){
-        if(confirm("Sicuro di voler eliminare questa task?")){
-            this.todo.splice(i, 1)
-        }
+    taskRemove(i) {
+      if (confirm("Sicuro di voler eliminare questa task?")) {
+        this.todo.splice(i, 1);
+      }
     },
     taskAdd() {
-        // creare oggetto
-        let objectTask = {
-            text: this.newTask,
-            done: false
-        }
-        this.todo.push(objectTask)
-        this.newTask = undefined
-    }
+      // creare oggetto
+      let objectTask = {
+        text: this.newTask,
+        done: false,
+      };
+      this.todo.push(objectTask);
+      this.newTask = undefined;
+    },
+    reverseBoolean(i) {
+      if (this.todo[i].done) {
+        this.todo[i].done = !this.todo[i].done // false
+      } else {
+        this.todo[i].done = !this.todo[i].done // true
+      }
+    },
   },
 }).mount("#app");
